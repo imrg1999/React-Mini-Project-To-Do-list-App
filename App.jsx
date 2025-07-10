21ea213c35b27ecd96d144fc2f3229f3bbc006ab
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-07-10 12:54:30 +0530

    Add files via upload
    Prevent the form from submission

--- App.jsx
+++ App.jsx
@@ -2,18 +2,25 @@ import React, { useState } from 'react'
 
 const App = () => {
   const[title, setTitle] = useState("");
-  const[description, setDescriptio] = useState("");  
+  const[description, setDescription] = useState(""); 
+  
+  const submitHandler =(e) => {
+    e.preventDefault();
+    setTitle("");
+    setDescription("");
+  }
+  
   return (
     <div>
       <h1 className='text-5xl text-center font-bold bg-blue-400 text-white p-10'>To-Do-List App</h1>
 
-      <form action="" className='flex justify-center'>
+      <form className='flex justify-center' onSubmit={submitHandler}>
 
         <input type="text" className='border-black text-2xl border-2 m-5 px-4 py-2' 
         placeholder='Enter The Task...'  onChange={(e)=> setTitle(e.target.value)} value={title}/>
 
         <input type="text" className='border-black text-2xl border-2 m-5 px-4 py-2' 
-        placeholder='Enter The Description....'  onChange={(e)=> setDescriptio(e.target.value)} value={description}/>
+        placeholder='Enter The Description....'  onChange={(e)=> setDescription(e.target.value)} value={description}/>
 
         <button className='bg-black text-white px-4 py-2 m-5 font-bold border-2 rounded'>Add Task</button>
       </form>
