e75b0ec5f19fd29daa7ea20ef8859179c79e72e3
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-07-10 16:58:44 +0530

    Add files via upload
    Upated with delete handling function

--- App.jsx
+++ App.jsx
@@ -12,14 +12,23 @@ const App = () => {
     setDescription("");
   }
 
+  const deleteHandling = (index) => {
+    let copyTask = [...mainTask];
+    copyTask.splice(index,1);
+    setMainTask(copyTask);
+  }
+
   let renderTask;
   if(mainTask.length === 0) {
     renderTask = <h1>No Tasks assigned!</h1> } 
     else { renderTask = mainTask.map((task, index)=> {
       return (
-      <li key={task.index}>
-        <h5>{task.title}</h5>
-        <h6>{task.description}</h6>
+      <li key={index}>
+        <div className='flex justify-between w-2/3'>
+        <h5 className='text-2xl font-bold'>{task.title}</h5>
+        <h6 className='text-xl font-bold'>{task.description}</h6>
+        <button className='bg-blue-800 text-white px-2 py-2 m-1 rounded' onClick={(index)=>{deleteHandling(index)}}>Delete</button>
+        </div>
       </li>)
     })}
   return (
@@ -34,7 +43,7 @@ const App = () => {
         <input type="text" className='border-black text-2xl border-2 m-5 px-4 py-2' 
         placeholder='Enter The Description....'  onChange={(e)=> setDescription(e.target.value)} value={description}/>
 
-        <button className='bg-black text-white px-4 py-2 m-5 font-bold border-2 rounded'>Add Task</button>
+        <button className='bg-blue-800 text-white px-4 py-2 m-5 font-bold border-2 rounded'>Add Task</button>
       </form>
       <div className='p-8 bg-blue-200 text-center'>
         <ul>{renderTask}</ul>
